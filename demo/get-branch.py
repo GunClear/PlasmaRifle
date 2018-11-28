c6c39cff6b6469afeac2a5563af0f6b70654f57a
Bryant Eisenbach <fubuloubu@gmail.com>
2018-11-27 21:25:04 -0500

    Added functionality to get branch stuff working!

--- demo/get-branch.py
+++ demo/get-branch.py
@@ -1,3 +1,107 @@
+from eth_typing import (
+        Address,
+    )
+
+from eth_utils import (
+        keccak,
+        to_bytes,
+        to_int,
+        to_canonical_address,
+    )
+
+from trie.smt import SparseMerkleProof
+
+from web3 import Web3
+
+
+def int_to_bytes32(value: int) -> bytes:
+    v = to_bytes(value).rjust(32, b'\x00')
+    return v
+
+
+EMPTY_NODE_HASHES = []
+node = keccak(int_to_bytes32(0))
+for _ in range(160):  # Tree depth
+    EMPTY_NODE_HASHES.insert(0, node)
+    node = keccak(EMPTY_NODE_HASHES[0] + EMPTY_NODE_HASHES[0])
+
+authlist_abi = [
+        {
+            'name': 'TreeUpdate',
+            'inputs': [
+                {'type': 'address', 'name': 'account', 'indexed': True},
+                {'type': 'uint256', 'name': 'status', 'indexed': True},
+                {'type': 'bytes32[160]', 'name': 'nodes', 'indexed': False}
+            ],
+            'anonymous': False,
+            'type': 'event',
+        },{
+            'name': 'status',
+            'outputs': [{'type': 'uint256', 'name': 'out'}],
+            'inputs': [{'type': 'address', 'name': 'arg0'}],
+            'constant': True,
+            'payable': False,
+            'type': 'function',
+        },{
+            'name': 'root',
+            'outputs': [{'type': 'bytes32', 'name': 'out'}],
+            'inputs': [],
+            'constant': True,
+            'payable': False,
+            'type': 'function'
+        }
+    ]
+
+
+class Listener:
+    """
+    The Listener listens for updates and synchronizes their
+    proof and value accordingly
+    """
+    def __init__(self, w3: Web3, acct: Address, tree_address: Address):
+        self._tree = w3.eth.contract(tree_address, abi=authlist_abi)
+        self.acct = acct
+        self._proof = SparseMerkleProof(
+                to_canonical_address(acct),
+                int_to_bytes32(0),
+                EMPTY_NODE_HASHES
+            )
+        self._filter = self._tree.events.TreeUpdate().createFilter(fromBlock=0)
+        # Iterate over all previously logged entries
+        for log in self._filter.get_all_entries():
+            self._proof.merge(
+                    to_canonical_address(log.args.account),
+                    int_to_bytes32(log.args.status),
+                    log.args.nodes
+                )
+
+    def sync(self):
+        # Iterate over last unchecked logs, update proof for them
+        for log in self._filter.get_new_entries():
+            self._proof.merge(
+                    to_canonical_address(log.args.account),
+                    int_to_bytes32(log.args.status),
+                    log.args.nodes
+                )
+
+    @property
+    def branch(self) -> list:
+        return self._proof.branch[:]  # shallow copy
+
+    @property
+    def updated(self) -> bool:
+        return self._proof.root_hash == self._tree.functions.root().call()
+
+    @property
+    def status(self) -> int:
+        self.sync()  # Validate that the value is up-to-date
+
+        # Validate that the proof is correct (and therefore matches tree)
+        status = to_int(self._proof.value)
+        assert self._tree.functions.status(self.acct).call() == status
+        return status
+
+
 from web3.auto.infura.ropsten import w3
 
 import json
@@ -16,9 +120,8 @@ ap.add_argument("account", type=str, \
 args = ap.parse_args()
 #TODO get 'w3' from args.network
 
-authlist = w3.eth.contract(address=args.authlist, **interface)
+listener = Listener(w3, args.account, args.authlist)
 
-from plasma_rifle import get_branch
-branch = get_branch(args.authlist, args.account)  # FIXME must obtain merkle branch!
+branch = listener.branch
 
-[print(node) for node in branch]  # Print in list format
+[print('0x'+node.hex()) for node in branch]  # Print in list format
